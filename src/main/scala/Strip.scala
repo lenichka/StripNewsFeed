0c02be0cbc7de8e8855f1e2c548362063db033ad
Helen Prask <helen.prask@gmail.com>
2016-04-13 12:05:26 -0700

    In progress: tests and types to update :)

--- src/main/scala/Strip.scala
+++ src/main/scala/Strip.scala
@@ -1,7 +1,71 @@
+import scala.collection.immutable.SortedMap
+
 /**
   * Created by helen on 4/9/2016.
   */
-class Strip {
+class Strip[T] ( val depth: Int, val hashKey:Any => Long){
 
-  
+  var currentMarker:Long = 0
+  var store = SortedMap.empty[Long,Any]
+  var storeHashKeys = SortedMap.empty[Long,Long] // (key = hashKey(element), element = store.Key)
+
+  def listToAdd(xs: List[Any]): List[Any] = {
+    val listToAdd = xs.filter(x => storeHashKeys.keySet.contains(hashKey(x)) == false)
+    println("inSet: " + xs)
+    println("setToAdd: " + listToAdd)
+    listToAdd
+  }
+
+  def add[T](xs: List[T]): Long =  xs match {
+    case List() => currentMarker
+    case z::zs => {
+      val addList = listToAdd(xs)
+      val listMap = (currentMarker + 1 to currentMarker + addList.length)zip(addList)
+      store = SortedMap( listMap: _*) ++ store
+      if (store.size > depth) store = store.drop(store.size-depth)
+
+      storeHashKeys = storeHashKeys ++ store.map{case(k,e) => (hashKey(e),k)}
+      currentMarker = currentMarker + addList.length
+      println("storeHashKeys: " + storeHashKeys)
+      println("store: " + store)
+      currentMarker
+    }
+  }
+
+  def getUp(count:Int, marker:Long): (List[Any],Long) =  {
+    var ret = SortedMap.empty[Long,Any]
+    if (marker < store.firstKey) {
+      ret = store.take(count)
+    }
+    if (marker > store.lastKey) {
+      ret = store.from(store.lastKey - count + 1)
+    }
+    else {
+      ret = store.from(marker).take(count)
+    }
+    println("ret: " + ret)
+    println("list: " + ret.toList.unzip._2)
+    println("new marker: " + ret.lastKey)
+    (ret.toList.unzip._2, ret.lastKey)
+  }
+
+  def getDown(count:Int, marker:Long): (List[Any],Long) =  {
+    var ret = SortedMap.empty[Long,Any]
+    if (marker < store.firstKey) {
+      ret = store.take(count)
+    }
+    if (marker > store.lastKey) {
+      ret = store.from(store.lastKey - count + 1)
+    }
+    else {
+      // may run over marker, force a new marker?!
+      ret = store.from(marker-count).take(count)
+    }
+    println("ret: " + ret)
+    println("list: " + ret.toList.unzip._2)
+    println("new marker: " + ret.lastKey)
+    (ret.toList.unzip._2, ret.lastKey)
+  }
+
 }
+
--- src/test/scala/StripSpec.scala
+++ src/test/scala/StripSpec.scala
@@ -5,12 +5,34 @@ class StripSpec extends FlatSpec with Matchers {
 // See manual here http://doc.scalatest.org/1.8/org/scalatest/FlatSpec.html
 
   "A Sample object" should "do nothing" in {
-    val sample = new Sample()
+    val sample = new Strip[String](7, x => x.toString.toLong)
     sample should be ( sample )
-  }
+    val myMarker1 = sample.add(List("100", "200", "300", "400"))
+    val myMarker2 = sample.add(List("200", "300", "700", "800"))
+    val myMarker3 = sample.add(List())
+    val myMarker4 = sample.add(List("900", "1000", "1100", "1200", "100"))
+    val myMarker = sample.add(List("1300", "1400", "1500", "1600", "700", "500", "400", "1700"))
+    println("current marker: " + myMarker)
+    println("store size: " + sample.store.size)
+    println("store elemets: " + sample.store)
+    val ret1 = sample.getUp(3,11)
+    val ret2 = sample.getUp(3,7)
+    val ret3 = sample.getUp(12,7)
+    val ret4 = sample.getUp(23,18)
+    val ret5 = sample.getUp(3,18)
 
-  it should "do nothing else" in {
-    val sample = new Sample()
-    sample should be ( sample )
+    val ret11 = sample.getDown(3,11)
+    val ret12 = sample.getDown(3,7)
+    val ret13 = sample.getDown(12,7)
+    val ret14 = sample.getDown(23,18)
+    val ret15 = sample.getDown(3,18)
+
   }
+/**
+  it should "do nothing else" in {
+    val sample = new Strip(10)
+    sample should be ( sample )
+    val myMarker = sample.add(List("500", "600", "700", "800"))
+  }
+ */
 }
