299ee9f6728a24f49c74a9d9bf8c75a75eabe381
Helen Prask <helen.prask@gmail.com>
2016-04-13 22:58:49 -0700

    Fixed types

--- src/main/scala/Strip.scala
+++ src/main/scala/Strip.scala
@@ -3,37 +3,31 @@ import scala.collection.immutable.SortedMap
 /**
   * Created by helen on 4/9/2016.
   */
-class Strip[T] ( val depth: Int, val hashKey:Any => Long){
+class Strip[T] ( val depth: Int, val hashFunction: T => Long){
 
   var currentMarker:Long = 0
-  var store = SortedMap.empty[Long,Any]
-  var storeHashKeys = SortedMap.empty[Long,Long] // (key = hashKey(element), element = store.Key)
+  var store = SortedMap.empty[Long, T]
+  var hashKeys = SortedMap.empty[Long, Long] // (key = hashKey(element), element = store.Key)
 
-  def listToAdd(xs: List[Any]): List[Any] = {
-    val listToAdd = xs.filter(x => storeHashKeys.keySet.contains(hashKey(x)) == false)
-    println("inSet: " + xs)
-    println("setToAdd: " + listToAdd)
-    listToAdd
-  }
+  def filter(xs: List[T]): List[T] = xs.filterNot(x => hashKeys.contains(hashFunction(x)))
 
-  def add[T](xs: List[T]): Long =  xs match {
+  def add(xs: List[T]): Long =  xs match {
     case List() => currentMarker
-    case z::zs => {
-      val addList = listToAdd(xs)
+    case z::zs =>
+      val addList = filter(xs)
       val listMap = (currentMarker + 1 to currentMarker + addList.length)zip(addList)
       store = SortedMap( listMap: _*) ++ store
       if (store.size > depth) store = store.drop(store.size-depth)
 
-      storeHashKeys = storeHashKeys ++ store.map{case(k,e) => (hashKey(e),k)}
+      hashKeys = hashKeys ++ store.map{case(k,e) => (hashFunction(e),k)}
       currentMarker = currentMarker + addList.length
-      println("storeHashKeys: " + storeHashKeys)
-      println("store: " + store)
+//      println("storeHashKeys: " + storeHashKeys)
+//      println("store: " + store)
       currentMarker
-    }
   }
 
-  def getUp(count:Int, marker:Long): (List[Any],Long) =  {
-    var ret = SortedMap.empty[Long,Any]
+  def getUp(count:Int, marker:Long): (List[T],Long) =  {
+    var ret = SortedMap.empty[Long, T]
     if (marker < store.firstKey) {
       ret = store.take(count)
     }
@@ -43,14 +37,14 @@ class Strip[T] ( val depth: Int, val hashKey:Any => Long){
     else {
       ret = store.from(marker).take(count)
     }
-    println("ret: " + ret)
-    println("list: " + ret.toList.unzip._2)
-    println("new marker: " + ret.lastKey)
+//    println("ret: " + ret)
+//    println("list: " + ret.toList.unzip._2)
+//    println("new marker: " + ret.lastKey)
     (ret.toList.unzip._2, ret.lastKey)
   }
 
-  def getDown(count:Int, marker:Long): (List[Any],Long) =  {
-    var ret = SortedMap.empty[Long,Any]
+  def getDown(count:Int, marker:Long): (List[T], Long) =  {
+    var ret = SortedMap.empty[Long, T]
     if (marker < store.firstKey) {
       ret = store.take(count)
     }
@@ -61,9 +55,9 @@ class Strip[T] ( val depth: Int, val hashKey:Any => Long){
       // may run over marker, force a new marker?!
       ret = store.from(marker-count).take(count)
     }
-    println("ret: " + ret)
-    println("list: " + ret.toList.unzip._2)
-    println("new marker: " + ret.lastKey)
+//    println("ret: " + ret)
+//    println("list: " + ret.toList.unzip._2)
+//    println("new marker: " + ret.lastKey)
     (ret.toList.unzip._2, ret.lastKey)
   }
 
